f2a08f75a28f3fa18a64985ab8c661b33e2b1933
Дмитрий <dimats2350@gmail.com>
2025-03-29 14:43:13 +0100

    images ready commit

--- bsp2_basics/images.py
+++ bsp2_basics/images.py
@@ -5,110 +5,119 @@ from PIL import Image
 import utils
 
 
-def read_img(inp:str) -> Image.Image:
+def read_img(inp: str) -> Image.Image:
     """
         Returns a PIL Image given by its input path.
     """
-    img =  Image.open(inp)
+    img = Image.open(inp)
     return img
 
-def convert(img:Image.Image) -> np.ndarray:
+
+def convert(img: Image.Image) -> np.ndarray:
     """
         Converts a PIL image [0,255] to a numpy array [0,1].
     """
     ### STUDENT CODE
     # TODO: Implement this function.
 
-	# NOTE: The following lines can be removed. They prevent the framework
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    out = np.zeros((512,512,3))
+    out = np.array(img) / 255
 
     ### END STUDENT CODE
     return out
 
-def switch_channels(img:np.ndarray) -> np.ndarray:
+
+def switch_channels(img: np.ndarray) -> np.ndarray:
     """
         Swaps the red and green channel of a RGB image given by a numpy array.
     """
     ### STUDENT CODE
     # TODO: Implement this function.
 
-	# NOTE: The following lines can be removed. They prevent the framework
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    out = np.zeros(img.shape)
+    out = img[:, :, [1, 0, 2]]
 
     ### END STUDENT CODE
 
     return out
 
-def image_mark_green(img:np.ndarray) -> np.ndarray:
+
+def image_mark_green(img: np.ndarray) -> np.ndarray:
     """
         returns a numpy-array (HxW) with 1 where the green channel of the input image is greater or equal than 0.7, otherwise zero.
     """
     ### STUDENT CODE
     # TODO: Implement this function.
 
-	# NOTE: The following lines can be removed. They prevent the framework
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    mask = np.zeros(img.shape)
+    mask = img[:, :, 1] >= 0.7
 
     ### END STUDENT CODE
 
     return mask
 
 
-def image_masked(img:np.ndarray, mask:np.ndarray) -> np.ndarray:
+def image_masked(img: np.ndarray, mask: np.ndarray) -> np.ndarray:
     """
         sets the pixels of the input image to zero where the mask is 1.
     """
     ### STUDENT CODE
     # TODO: Implement this function.
 
-	# NOTE: The following lines can be removed. They prevent the framework
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
-
-    out = np.zeros(img.shape)
+    out = img.copy()
+    out[mask] = 0
 
     ### END STUDENT CODE
 
     return out
 
-def grayscale(img:np.ndarray) -> np.ndarray:
+
+def grayscale(img: np.ndarray) -> np.ndarray:
     """
         Returns a grayscale image of the input. Use utils.rgb2gray().
     """
     ### STUDENT CODE
     # TODO: Implement this function.
 
-	# NOTE: The following lines can be removed. They prevent the framework
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    out = np.zeros(img.shape)
+    out = utils.rgb2gray(img)
 
     ### END STUDENT CODE
 
     return out
 
-def cut_and_reshape(img_gray:np.ndarray) -> np.ndarray:
+
+def cut_and_reshape(img_gray: np.ndarray) -> np.ndarray:
     """
         Cuts the image in half (x-dim) and stacks it together in y-dim.
     """
     ### STUDENT CODE
     # TODO: Implement this function.
 
-	# NOTE: The following lines can be removed. They prevent the framework
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    out = np.zeros(img_gray.shape)
+    out = np.vstack((
+        img_gray[:, img_gray.shape[0] // 2:],
+        img_gray[:, 0:img_gray.shape[0] // 2]
+    ))
 
     ### END STUDENT CODE
 
     return out
 
-def filter_image(img:np.ndarray) -> np.ndarray:
+
+def filter_image(img: np.ndarray) -> np.ndarray:
     """
         filters the image with the gaussian kernel given below. 
     """
@@ -117,27 +126,49 @@ def filter_image(img:np.ndarray) -> np.ndarray:
     ### STUDENT CODE
     # TODO: Implement this function.
 
-	# NOTE: The following lines can be removed. They prevent the framework
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    out = np.zeros(img.shape)
+    padded_img = np.pad(img, ((2, 2), (2, 2), (0, 0)), mode='constant')
+    padded_img = padded_img.transpose(2, 0, 1)
+
+    color, height_pad, wight_pad = padded_img.shape
+
+    shape = (color, height_pad - 4, wight_pad - 4, 5, 5)
+    strides = (
+        padded_img.strides[0],
+        padded_img.strides[1],
+        padded_img.strides[2],
+        padded_img.strides[1],
+        padded_img.strides[2]
+    )
+
+    windows = np.lib.stride_tricks.as_strided(padded_img, shape=shape, strides=strides)
+
+    gaussian = gaussian.reshape(1, 1, 1, 5, 5)
+    out = np.sum(windows * gaussian, axis=(-1, -2)).transpose(1, 2, 0)
 
     ### END STUDENT CODE
 
     return out
 
-def horizontal_edges(img:np.ndarray) -> np.ndarray:
+
+def horizontal_edges(img: np.ndarray) -> np.ndarray:
     """
         Defines a sobel kernel to extract horizontal edges and convolves the image with it.
     """
     ### STUDENT CODE
     # TODO: Implement this function.
 
-	# NOTE: The following lines can be removed. They prevent the framework
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    out = np.zeros(img.shape)
-
+    sobel_filter = np.array([
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1]
+    ])
+    out = scipy.ndimage.correlate(img, sobel_filter, mode='constant')
     ### END STUDENT CODE
 
     return out
